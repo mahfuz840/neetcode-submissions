6c426f64fe9d7d49551000ab904ee07d12bad494
Mahfuz Ahmed Masum <43561160+mahfuz840@users.noreply.github.com>
2026-08-08 11:31:37 +0600

    Add: three-integer-sum - submission-3

---
+++ Data Structures & Algorithms/three-integer-sum/submission-3.py
@@ -0,0 +1,27 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        ans = []
+        uniq = set()
+
+        nums.sort()
+
+        for i, x in enumerate(nums):
+            target = 0 - x
+            j = i + 1
+            k = len(nums) - 1
+            while j < k:
+                sm = nums[j] + nums[k]
+                if sm == target:
+                    arr = [nums[i], nums[j], nums[k]]
+                    uniq_str = str(nums[i]) + str(nums[j]) + str(nums[k])
+                    if uniq_str not in uniq:
+                        ans.append(arr)
+                        uniq.add(uniq_str)
+                    k -= 1
+                elif sm < target:
+                    j += 1
+                else:
+                    k -= 1
+        
+        return ans
+
