852411fee53c591b77a77a0ff75f4c1613572997
Mahfuz Ahmed Masum <43561160+mahfuz840@users.noreply.github.com>
2026-08-14 21:58:58 +0600

    Add: minimum-stack - submission-4

---
+++ Data Structures & Algorithms/minimum-stack/submission-4.py
@@ -0,0 +1,21 @@
+class MinStack:
+
+    def __init__(self):
+        self.st = []
+        self.minst = []
+
+    def push(self, val: int) -> None:
+        self.st.append(val)
+        if not self.minst or self.minst[-1] >= val:
+            self.minst.append(val)
+
+    def pop(self) -> None:
+        val = self.st.pop()
+        if self.minst[-1] == val:
+            self.minst.pop()
+
+    def top(self) -> int:
+        return self.st[-1]
+
+    def getMin(self) -> int:
+        return self.minst[-1]
