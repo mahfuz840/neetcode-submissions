eb6d302e246280443afce146f9eab2d9c4dce646
Mahfuz Ahmed Masum <43561160+mahfuz840@users.noreply.github.com>
2026-08-17 08:26:54 +0600

    Add: car-fleet - submission-6

---
+++ Data Structures & Algorithms/car-fleet/submission-6.py
@@ -0,0 +1,28 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        tuples = []
+        for i in range(len(position)):
+            tuples.append((position[i], speed[i]))
+        
+        tuples_sorted = sorted(tuples, reverse = True)
+
+        st = []
+        ans = 0
+        mx = -1
+
+        for tup in tuples_sorted:
+            rem = target - tup[0]
+            rem_time = rem / tup[1]
+
+            new_fleet = False
+            while st and mx < rem_time:
+                new_fleet = True
+                st.pop()
+            
+            if new_fleet:
+                ans += 1
+            
+            st.append(rem_time)
+            mx = max(mx, rem_time)
+        
+        return ans + 1
